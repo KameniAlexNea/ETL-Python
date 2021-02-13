64f2a44533e1b83e56bc151e949acd2da8f8f293
kamenialexnea <kamenialexnea@gmail.com>
2021-02-13 02:27:36 +0100

    Connexion à une base de données oracle

--- BI_eyes_django/eyes/oracle.py
+++ BI_eyes_django/eyes/oracle.py
@@ -7,7 +7,7 @@ query2 = "select class, avg(age) from students group by class"
 conn = pt.open_connection("config.yml")
 
 ## passing the conn object to the query_to_df
-df1 = pt.query_to_df(query, conn, 10000)
+df1 = pt.query_to_df(query1, conn, 10000)
 
 ## passing the conn object to the query_to_df , without to open again
 df2 = pt.query_to_df(query2, conn, 10)
